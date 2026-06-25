9c2a8d2f6e5c08e9737db78f3a23e490c095fa7e
Sasmita32029 <2300032029@kluniversity.in>
2026-06-25 18:35:22 +0530

    Completed HandsOn 3 Components Input Output

--- src/app/pages/course-list/course-list.ts
+++ src/app/pages/course-list/course-list.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card/course-card';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CourseCard],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+export class CourseList {
+
+  selectedCourseId = 0;
+
+  courses = [
+    {
+      id: 1,
+      name: 'Angular',
+      code: 'ANG101',
+      credits: 4
+    },
+    {
+      id: 2,
+      name: 'Java',
+      code: 'JAVA101',
+      credits: 3
+    },
+    {
+      id: 3,
+      name: 'SQL',
+      code: 'SQL101',
+      credits: 2
+    }
+  ];
+
+  onEnroll(id: number) {
+    this.selectedCourseId = id;
+  }
+}
